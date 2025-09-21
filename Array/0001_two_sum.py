3e103285592fc7792069ea0279347221a8c1549d
Sughosh Kulkarni <sjce.sughosh@gmail.com>
2025-09-21 19:42:42 +0530

    Added comment

--- Array/0001_two_sum.py
+++ Array/0001_two_sum.py
@@ -2,6 +2,6 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
         for i in range(n):
-            for j in range(i+1,n):
+            for j in range(i+1,n): # To prevent duplicate
                 if target == nums[i]+nums[j]:
-                    return [i,j]
+                    return [i,j] # Returned value is output in leetcode, not like codeforces
