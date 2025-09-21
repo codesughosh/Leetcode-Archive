86cf4bf97965a88e5a18b053ed90708146fbad31
Sughosh Kulkarni <sjce.sughosh@gmail.com>
2025-09-21 19:38:32 +0530

    Added comment

--- Array/0035_seach_insert.py
+++ Array/0035_seach_insert.py
@@ -1,6 +1,6 @@
 import bisect
-
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
+        # Index stores the target index if found or it stores the index where the target would be if present
         index = bisect.bisect_left(nums, target)
         return index
