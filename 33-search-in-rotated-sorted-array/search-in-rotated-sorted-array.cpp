f7fe6d3d44e239e58194acc46302b8859f299b33
Sughosh Kulkarni <sjce.sughosh@gmail.com>
2026-08-03 21:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (36.84%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,31 +2,49 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int left = 0;
-        int right = nums.size() - 1;
+        int n = nums.size() - 1;
+        int right = n;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        while(left<right)
+        {
+            int mid = left + (right-left)/2;
 
-            if (nums[mid] == target) {
+            if(nums[mid]>nums[right])
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid;
+            }
+        }
+
+        if (target >= nums[left] && target <= nums[n])
+        {
+            right=n;
+        }
+        else
+        {
+            right=left-1;
+            left=0;
+        }
+
+
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+
+            if(nums[mid]==target)
+            {
                 return mid;
             }
-
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                }
-                else {
-                    left = mid + 1;
-                }
+            else if(target>nums[mid])
+            {
+                left=mid+1;
             }
-
-            else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                }
-                else {
-                    right = mid - 1;
-                }
+            else
+            {
+                right=mid-1;
             }
         }
 
